1bc0195b2b64a7f34b424577410031f883629686
Jamess222 <95212211+Jamess222@users.noreply.github.com>
2021-12-07 14:08:55 +0100

    Add files via upload

---
+++ IClown.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    interface IClown
+    {
+        string FunnyThing { get;  }
+        void Honk();
+        
+        interface IScaryClown : IClown
+        {
+            string ScaryThingIHave { get; }
+            void ScareLittleChildren();
+        }
+        class FunnyFunny : IClown
+        {
+            public FunnyFunny(string funnyThingIHave)
+            {
+                this.funnyThingIHave = funnyThingIHave;
+            }
+            private string funnyThingIHave
+            {
+                get { return "HI! + " + funnyThingIHave; }
+            }
+            public void Honk()
+            {
+                Console.WriteLine(this.funnyThingIHave);
+            }
+        }
+
+        class ScaryScary : FunnyFunny, IScaryClown
+        {
+            public ScaryScary(string funnyThingIHave, int numberOfScaryThings) : base(funnyThingsIHave)
+            {
+                this.numberOfScaryThings = numberOfScaryThings;
+            }
+
+            private int numberOfScaryThings;
+            public string ScaryThingsIHave
+            {
+                get { return "Mam " + numberOfScaryThings + " pajakow"; }
+            }
+
+            public void ScareLittleChildren()
+            {
+                Console.WriteLine("BU XD");
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy guy = new TallGuy()
+            {
+                height = 175,
+                name = "James"
+            };
+            guy.Talk();
+            guy.Honk();
+        }
+    }
+}
